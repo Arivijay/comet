bd7a6d5484a99ef4e551a63ae5276899a7af483c
vijay-ramp <vijay.panneerselvam@liveramp.com>
2025-10-06 22:23:56 -0400

    fix(eks): multiline HCL variable blocks + fmt

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,7 +1,34 @@
-variable "aws_region" { type = string default = "us-east-1" }
-variable "cluster_name" { type = string default = "comet-hello-eks" }
-variable "cluster_version" { type = string default = "1.29" }
-variable "node_instance_types" { type = list(string) default = ["t3.small"] }
-variable "desired_size" { type = number default = 2 }
-variable "min_size" { type = number default = 2 }
-variable "max_size" { type = number default = 4 }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "comet-hello-eks"
+}
+
+variable "cluster_version" {
+  type    = string
+  default = "1.29"
+}
+
+variable "node_instance_types" {
+  type    = list(string)
+  default = ["t3.small"]
+}
+
+variable "desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "min_size" {
+  type    = number
+  default = 2
+}
+
+variable "max_size" {
+  type    = number
+  default = 4
+}
